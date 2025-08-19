ec029a533e4e7931c03dc40f7fd31d11fac408d5
Junaid Afzal <junaidafzal2013@gmail.com>
2025-08-19 16:53:57 +0500

    feat: add Lenis for smooth scrolling and implement AnimatedHeaderSection in Hero and Services components

---
+++ src/components/AnimatedHeaderSection.jsx
@@ -0,0 +1,56 @@
+import { useRef } from "react";
+import AnimatedTextLines from "../components/AnimatedTextLines";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+const AnimatedHeaderSection = ({ subtitle, title, text, textColor, withScrollTrigger = false }) => {
+    const contextRef = useRef(null);
+    const headerRef = useRef(null);
+
+    useGSAP(() => {
+        const tl = gsap.timeline({
+            scrollTrigger: withScrollTrigger ? {
+                trigger: contextRef.current,
+            } : undefined
+        });
+        tl.from(contextRef.current, {
+            y: "50vh",
+            duration: 1,
+            ease: 'circ.out',
+        })
+        tl.from(headerRef.current, {
+            y: 200,
+            opacity: 0,
+            duration: 1,
+            ease: 'circ.out',
+        }, "<+0.2")
+        tl.from(contextRef.current, {
+            scale: 0.8,
+            duration: 1,
+            ease: 'back.out',
+        }, "<")
+    }, []);
+    return (
+        <div ref={contextRef} >
+            <div className="" style={{ clipPath: "polygon(0 0, 100% 0%, 100% 100%, 0% 100%)" }}>
+                <div ref={headerRef} className='flex flex-col justify-center gap-12 pt-16 sm:gap-16 '>
+                    <p className={`text-sm font-light tracking-[0.5rem] uppercase px-10 ${textColor}`}>{subtitle}</p>
+                    <div className="px-10">
+                        <h1 className={`flex flex-col flex-wrap uppercase gap-12 ${textColor} banner-text-responsive sm:gap-16 md:block py-4`}>
+                            {title}
+                        </h1>
+                    </div>
+                </div>
+            </div>
+            <div className={`relative px-10 ${textColor}`}>
+                <div className="absolute inset-x-0 border-t-2" />
+                <div className="py-12 sm:py-16 text-end">
+                    <AnimatedTextLines text={text} className={`font-light uppercase value-text-responsive ${textColor}`} />
+
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AnimatedHeaderSection
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,60 +1,18 @@
-import { useRef } from "react";
-import AnimatedTextLines from "../components/AnimatedTextLines";
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
+
 import { Canvas } from "@react-three/fiber";
 import { Planet } from "../components/Planet";
 import { Environment, Float, Lightformer } from "@react-three/drei";
 import { useMediaQuery } from "react-responsive";
+import AnimatedHeaderSection from "../components/AnimatedHeaderSection";
 
 const Hero = () => {
     const isMobile = useMediaQuery({ maxWidth: 853 });
-    const contextRef = useRef(null);
-    const headerRef = useRef(null);
-    const aboutText = 'I build sleek digital experiences\n from mobile to web\n where design meets performance';
+    const text = 'I build sleek digital experiences\n from mobile to web\n where design meets performance';
 
-    useGSAP(() => {
-        const tl = gsap.timeline();
-        tl.from(contextRef.current, {
-            y: "50vh",
-            duration: 1,
-            ease: 'circ.out',
-        })
-        tl.from(headerRef.current, {
-            y: 200,
-            opacity: 0,
-            duration: 1,
-            ease: 'circ.out',
-        }, "<+0.2")
-        tl.from(contextRef.current, {
-            scale: 0.8,
-            duration: 1,
-            ease: 'back.out',
-        }, "<")
-    }, []);
 
     return (
         <section id='home' className='flex flex-col justify-end min-h-screen '>
-            <div ref={contextRef} >
-                <div className="" style={{ clipPath: "polygon(0 0, 100% 0%, 100% 100%, 0% 100%)" }}>
-                    <div ref={headerRef} className='flex flex-col justify-center gap-12 pt-16 sm:gap-16 '>
-                        <p className="text-sm font-light tracking-[0.5rem] uppercase px-10 text-black">404 No Bugs Found</p>
-                        <div className="px-10">
-                            <h1 className="flex flex-col flex-wrap uppercase gap-12 text-black banner-text-responsive sm:gap-16 md:block py-4">
-                                Junaid Afzal
-                            </h1>
-                        </div>
-                    </div>
-                </div>
-                <div className="relative px-10 text-black ">
-                    <div className="absolute inset-x-0 border-t-2" />
-                    <div className="py-12 sm:py-16 text-end">
-                        <AnimatedTextLines text={aboutText} className="font-light uppercase value-text-responsive" />
-
-
-                    </div>
-                </div>
-            </div>
+            <AnimatedHeaderSection subtitle={"404 No Bugs Found"} title={"Junaid Afzal"} text={text} textColor={"text-black"} />
             <figure className="absolute inset-0  -z-50 " style={{ width: "100vw", height: "100vh" }}>
                 <Canvas shadows camera={{ position: [0, 0, -10], fov: 17.5, near: 1, far: 20 }}>
                     <ambientLight intensity={0.5} />
