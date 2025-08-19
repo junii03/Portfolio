313ba89e440e3310389390b0f0d16137c8ffbae2
Junaid Afzal <junaidafzal2013@gmail.com>
2025-08-19 11:56:00 +0500

    feat: integrate 3D planet model and responsive canvas in Hero section
    - Added @react-three/fiber and @react-three/drei dependencies for 3D rendering.
    - Implemented a Canvas in the Hero section to display a 3D planet model.
    - Created a new Planet component to handle the 3D model and animations.
    - Utilized useMediaQuery for responsive scaling of the planet based on screen size.
    - Enhanced animations using GSAP for smooth transitions and rotations of the planet and its rings.

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,11 +2,16 @@ import { useRef } from "react";
 import AnimatedTextLines from "../components/AnimatedTextLines";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { Canvas } from "@react-three/fiber";
+import { Planet } from "../components/Planet";
+import { Environment, Float, Lightformer } from "@react-three/drei";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+    const isMobile = useMediaQuery({ maxWidth: 853 });
     const contextRef = useRef(null);
     const headerRef = useRef(null);
-    const aboutText = 'I build sleek, app-like digital experiences\n from mobile to web\n where design meets performance.';
+    const aboutText = 'I build sleek digital experiences\n from mobile to web\n where design meets performance';
 
     useGSAP(() => {
         const tl = gsap.timeline();
@@ -50,6 +55,41 @@ const Hero = () => {
                     </div>
                 </div>
             </div>
+            <figure className="absolute inset-0  -z-50 " style={{ width: "100vw", height: "100vh" }}>
+                <Canvas shadows camera={{ position: [0, 0, -10], fov: 17.5, near: 1, far: 20 }}>
+                    <ambientLight intensity={0.5} />
+                    <Float speed={0.5}>
+                        <Planet scale={isMobile ? 0.7 : 1} />
+                    </Float>
+
+
+
+                    <Environment resolution={256}>
+                        <group rotation={[-Math.PI / 3, 4, 1]} >
+                            <Lightformer
+                                form={"circle"}
+                                intensity={2}
+                                scale={10}
+                                position={[0, 3, 1]}
+                            />
+                            <Lightformer
+                                form={"circle"}
+                                intensity={2}
+                                scale={10}
+                                position={[-5, -1, -1]}
+                            />
+                            <Lightformer
+                                form={"circle"}
+                                intensity={2}
+                                scale={16}
+                                position={[10, 1, 0]}
+                            />
+                        </group>
+                    </Environment>
+
+
+                </Canvas>
+            </figure>
         </section>
     )
 }
